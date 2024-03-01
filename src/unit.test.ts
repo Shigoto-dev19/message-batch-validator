b2bfe41e9355eb69a4e9fabc5e8792366e939d84
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2024-03-01 22:28:12 +0100

    Add unit tests for message details validation

---
+++ src/unit.test.ts
@@ -0,0 +1,94 @@
+import { Field, Bool } from 'o1js';
+import { MessageDetails } from './batch';
+
+// Unit tests for Message Details Validation
+describe('Message Details Validation Tests', () => {
+  it('should return invalid for agent ID out of range', () => {
+    const messageDetails = new MessageDetails({
+      agentId: Field(3001),
+      agentXLocation: Field(1300),
+      agentYLocation: Field(12700),
+      checkSum: Field(17001),
+    });
+    const checkField = MessageDetails.validateNonAdmin(messageDetails);
+    const idCheck = checkField.toBits(5)[0];
+
+    expect(idCheck).toEqual(Bool(false));
+  });
+
+  it('should return invalid for agent xLocation out of range', () => {
+    const messageDetails = new MessageDetails({
+      agentId: Field(1000),
+      agentXLocation: Field(15001),
+      agentYLocation: Field(15500),
+      checkSum: Field(31501),
+    });
+    const checkField = MessageDetails.validateNonAdmin(messageDetails);
+    const xLocationCheck = checkField.toBits(5)[1];
+
+    expect(xLocationCheck).toEqual(Bool(false));
+  });
+
+  it('should return invalid for agent yLocation out of range', () => {
+    const messageDetails = new MessageDetails({
+      agentId: Field(1000),
+      agentXLocation: Field(2000),
+      agentYLocation: Field(30000),
+      checkSum: Field(33000),
+    });
+    const checkField = MessageDetails.validateNonAdmin(messageDetails);
+    const yLocationCheck = checkField.toBits(5)[2];
+
+    expect(yLocationCheck).toEqual(Bool(false));
+  });
+
+  it('should return invalid for agent YLocation < XLocation', () => {
+    const messageDetails = new MessageDetails({
+      agentId: Field(1000),
+      agentXLocation: Field(6000),
+      agentYLocation: Field(5000),
+      checkSum: Field(12000),
+    });
+    const checkField = MessageDetails.validateNonAdmin(messageDetails);
+    const xyCheck = checkField.toBits(5)[3];
+
+    expect(xyCheck).toEqual(Bool(false));
+  });
+
+  it('should return invalid for a false checksum', () => {
+    const messageDetails = new MessageDetails({
+      agentId: Field(1000),
+      agentXLocation: Field(5000),
+      agentYLocation: Field(6000),
+      checkSum: Field(13000),
+    });
+    const checkField = MessageDetails.validateNonAdmin(messageDetails);
+    const checkSumCheck = checkField.toBits(5)[4];
+
+    expect(checkSumCheck).toEqual(Bool(false));
+  });
+
+  it('should return valid for agent ID is zero', () => {
+    const messageDetails = new MessageDetails({
+      agentId: Field(0),
+      agentXLocation: Field(5000),
+      agentYLocation: Field(6000),
+      checkSum: Field(13000),
+    });
+    const isValid = () => MessageDetails.validate(messageDetails);
+
+    expect(isValid).not.toThrow();
+  });
+
+  it('should throw an error for an invalid message details', () => {
+    const messageDetails = new MessageDetails({
+      agentId: Field(1),
+      agentXLocation: Field(5000),
+      agentYLocation: Field(6000),
+      checkSum: Field(13000),
+    });
+    const isValid = () => MessageDetails.validate(messageDetails);
+
+    expect(isValid).toThrow();
+  });
+});
